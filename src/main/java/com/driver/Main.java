8937ac6d2de3f903b2d3751fe85092a2f2e3e9ef
svenkatesh0090 <venkateshcsp9@gmail.com>
2023-05-11 10:47:15 +0530

    Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,18 @@
 package com.driver;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=sc.nextInt();
+        Product p=new Product();
+        System.out.println(p.product(x,y));
+        System.out.println(p.product(x,y,z));
+        System.out.println(p.product((double)x,(double)y));
+    }
+
 
 }
